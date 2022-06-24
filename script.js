b9cdc278e08c80efd4d230e05040dbb8f8377929
AMD <michojekunle1@gmail.com>
2022-06-24 08:42:55 -0700

    studying dom

---
+++ script.js
@@ -0,0 +1,20 @@
+let val;
+
+val = window.innerHeight;
+val = window.innerWidth;
+
+//scroll points
+val = window.scrollY;
+val = window.scrollX;
+
+//location
+val = window.location;
+val = window.location.hostname;
+val = window.location.host;
+// window.location.href = "https://google.com";
+val = window.history.length;
+val = window.navigator.language;
+val = window.document;
+
+
+console.log(val);
